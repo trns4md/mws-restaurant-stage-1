08635113d5a6d5e9ef27e7b6ccbbf2a9742e14ec
Becky White <trns4md@charter.net>
2018-09-03 10:35:52 -0400

    Made code corrections from review

--- js/main.js
+++ js/main.js
@@ -160,10 +160,11 @@ createRestaurantHTML = (restaurant) => {
     li.className = 'list-item';
     const image = document.createElement('img');
     image.className = 'restaurant-img';
+    image.alt = 'Image of ' + restaurant.name;
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement('h2');
     name.innerHTML = restaurant.name;
     li.append(name);
 
@@ -214,7 +215,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     } */
     /**Adding and installing Service Worker */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/js/sw.js')
+    navigator.serviceWorker.register('/sw.js')
         .then(function(reg) {
             // registration worked
             console.log('Registration succeeded. Scope is ' + reg.scope);
